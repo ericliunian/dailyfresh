d34152c3aba7cda1cd4a8d6d6f51a1691f256c87
LiuNian666 <liunianbest@163.com>
2018-04-16 19:43:09 +0800

    增加商品信息

--- dailyfresh/dailyfresh/settings.py
+++ dailyfresh/dailyfresh/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'df_user',
+    'df_goods',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -108,3 +109,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+MDDIA_ROOT = os.path.join(BASE_DIR,'static')
